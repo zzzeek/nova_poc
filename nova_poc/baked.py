baba47c8ecef35bf6ce055f8effe5f4c3c260e00
Mike Bayer <mike_mp@zzzcomputing.com>
2014-06-29 01:09:43 -0400

    - improve baked to handle subquery eager loaders

--- nova_poc/baked.py
+++ nova_poc/baked.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm.query import QueryContext
+from sqlalchemy.orm.query import QueryContext, Query
 
 class BakedQuery(object):
     """an object that can produce a 'baked' Query, that is one where
@@ -33,11 +33,30 @@ class BakedQuery(object):
         self.steps.append(fn)
         return self
 
+    def _bake_subquery_loaders(self, context):
+        context.attributes['baked_queries'] = baked_queries = []
+        for k, v in context.attributes.items():
+            if isinstance(v, Query):
+                if 'subquery' in k:
+                    bk = BakedQuery(lambda *args: v)
+                    bk._cache_key = self._cache_key + k
+                    bk._bake()
+                    baked_queries.append((k, bk._cache_key, v))
+                del context.attributes[k]
+
+    def _unbake_subquery_loaders(self, context):
+        for k, cache_key, query in context.attributes["baked_queries"]:
+            bk = BakedQuery(lambda: query.with_session(context.session))
+            bk._params = self._params
+            bk._cache_key = cache_key
+            context.attributes[k] = bk
+
     def _bake(self):
         query = self.query
         for step in self.steps:
             query = step(query)
         context = query._compile_context()
+        self._bake_subquery_loaders(context)
         del context.session
         del context.query
         self._bakery[self._cache_key] = context
@@ -62,6 +81,8 @@ class BakedQuery(object):
         context.query = query
         context.session = query.session
         context.attributes = context.attributes.copy()
+
+        self._unbake_subquery_loaders(context)
 
         context.statement.use_labels = True
         if query._autoflush and not query._populate_existing:
